e1a14e5d3f015245d03d23a424ddcc55e51a9a19
Slotheron <32925757+Slotheron@users.noreply.github.com>
2018-02-07 07:50:59 +0100

    Changed namespace for WebAPI in RestAPI and TicketShop

--- src/WebApplication3/Program.cs
+++ src/WebApplication3/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace WebApplication3
+namespace TicketShop
 {
     public class Program
     {
