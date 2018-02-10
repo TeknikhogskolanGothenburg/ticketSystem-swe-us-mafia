d63dd3df84aa2229f5f80989194bc83773fadd14
Sofia Zaid <sofiazaid@gmail.com>
2018-02-10 22:12:58 +0100

    minor addition, added one more controller class to restapi: 'ticketeventdatecontroller', to be implemented

---
+++ src/WebApplication1/Controllers/TicketEventDateController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTapi.Controllers
+{
+    /// <summary>
+    /// Här får vi hantera att lägga in ett event på ett specifikt datum och en specifik venue tror jag.
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/TicketEventDate")]
+    public class TicketEventDateController : Controller
+    {
+        // GET: api/TicketEventDate
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/TicketEventDate/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/TicketEventDate
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/TicketEventDate/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
