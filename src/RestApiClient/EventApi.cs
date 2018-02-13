7f6418a4b9d896c00045f72dd6bfcaddac4b9ee4
SonnyAasen <35238145+SonnyAasen@users.noreply.github.com>
2018-02-13 13:27:21 +0100

    Added additional methods to Event/VenueApi classes in RestApiClient

---
+++ src/RestApiClient/EventApi.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestSharp;
+using TicketSystem.RestApiClient.Model;
+
+namespace TicketSystem.RestApiClient
+{
+    class EventApi
+    {
+        // TicketEvent refers to a model in class library
+        public List<TicketEvent> GetAllEvents()
+        {
+            var client = new RestClient("http://localhost:18001/");
+            var request = new RestRequest("Event", Method.GET);
+            var response = client.Execute<List<TicketEvent>>(request);
+            return response.Data;
+        }
+
+        public TicketEvent GetEventByQuery(string query)
+        {
+            var client = new RestClient("http://localhost:18001/");
+            var request = new RestRequest("Venue/{query}", Method.GET);
+            request.AddUrlSegment("query", query);
+            var response = client.Execute<TicketEvent>(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                throw new KeyNotFoundException(string.Format("Event with query {0} not found", query));
+            }
+
+            return response.Data;
+        }
+
+        public TicketEvent GetEventById(int EventId)
+        {
+            var client = new RestClient("http://localhost:18001/");
+            var request = new RestRequest("Event/{id}", Method.GET);
+            request.AddUrlSegment("id", EventId);
+            var response = client.Execute<TicketEvent>(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                throw new KeyNotFoundException(string.Format("Event with ID: {0} not found", EventId));
+            }
+
+            return response.Data;
+        }
+
+        public void AddNewEvent()
+        {
+            var client = new RestClient("http://localhost:18001/");
+            var request = new RestRequest("Event", Method.POST);
+            var response = client.Execute(request);
+        }
+
+        public void UpdateEvent()
+        {
+            var client = new RestClient("http://localhost:18001/");
+            var request = new RestRequest("Venue", Method.PUT);
+            var response = client.Execute(request);
+        }
+
+        public void DeleteEvent()
+        {
+            var client = new RestClient("http://localhost:18001/");
+            var request = new RestRequest("Event", Method.DELETE);
+            var response = client.Execute(request);
+        }
+    }
+}
