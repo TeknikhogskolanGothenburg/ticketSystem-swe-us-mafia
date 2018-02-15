cc6b18017fca477bde640829cb57d986dee5a64e
SonnyAasen <35238145+SonnyAasen@users.noreply.github.com>
2018-02-15 16:20:56 +0100

    Added SQL database script

---
+++ src/DbUp/Scripts/scripts0002.sql
@@ -0,0 +1,7 @@
+﻿ALTER TABLE TicketTransactions -- Table Name
+ADD BuyerEmailAddress nvarchar(50); -- Column Name datatype
+
+-- If you want to add a change to the database
+-- create a new script in the same folder this is in
+-- text file named scripts0003.sql
+-- 
