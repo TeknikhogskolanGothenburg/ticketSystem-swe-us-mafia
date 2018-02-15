c3d42eb0b25d3c7f8550d02814e82d8728256d0d
SonnyAasen <35238145+SonnyAasen@users.noreply.github.com>
2018-02-15 13:26:17 +0100

    added get all method to restapiclient for order

--- src/RestApiClient/OrderAdministratorApi.cs
+++ src/RestApiClient/OrderAdministratorApi.cs
@@ -7,6 +7,12 @@ namespace TicketSystem.RestApiClient
 {
     public class OrderAdministratorApi
     {
-        
+        public List<Order> GetAllOrders()
+        {
+            var client = new RestClient("http://localhost:51775/");
+            var request = new RestRequest("Order", Method.GET);
+            var response = client.Execute<List<Order>>(request);
+            return response.Data;
+        }
     }
 }
