1693d88c98e75de44c27f7ae3c1e17e751bd7ad1
SonnyAasen <35238145+SonnyAasen@users.noreply.github.com>
2018-02-16 09:09:19 +0100

    fixed script0003 so it runs

--- src/DbUp/Scripts/scripts0002.sql
+++ src/DbUp/Scripts/scripts0002.sql
@@ -4,4 +4,3 @@ ADD BuyerEmailAddress nvarchar(50); -- Column Name datatype
 -- If you want to add a change to the database
 -- create a new script in the same folder this is in
 -- text file named scripts0003.sql
--- 
